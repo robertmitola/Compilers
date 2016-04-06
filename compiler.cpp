395e4c51977893619e14248b3260fb19522f413a
robertmitola <robert.mitola1@marist.edu>
2016-04-06 18:19:58 -0400

    Multiple Programs in One Text File
    Compiler can now compile multiple programs written in the same text file.

--- compiler.cpp
+++ compiler.cpp
@@ -9,6 +9,7 @@ CMPT 432 - DESIGN OF COMPILERS
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <regex>
 #include <sstream>
 #include <iomanip>
 #include <queue>
@@ -21,17 +22,30 @@ using std::string;
 using std::queue;
 using std::setw;
 using std::cout;
-
+/*
+// C++ implementation of PHP's explode
+// for splitting the source file at every $ so one can compile multiple programs
+// source	: the source string 
+// returns	: a queue of separate programs (strings) to compile
+queue<string> explode(string const & source)
+{
+	queue<string> programs;
+	istringstream iss(source);
+	for(string prog; getline(iss, prog, '$'))
+		programs.push(move(prog));
+	return programs;
+}
+*/
 int main(int argc, char *argv[])
 {
 	////////// SETUP ////////////////////////////////////////////////
 	
-	string source; // variable to store the source program filepath
+	string fileName; // variable to store the source program filepath
+	ifstream sourceFile; // variable to store the filestream
 	bool verbose = false; // true if verbose output should occur
-	cout << argv[2] << endl;
 	if(argc > 1) // if the user entered a program filepath for compiling
 	{
-		source = argv[1]; // set the source variable to that program filepath
+		fileName = argv[1]; // set the source variable to that program filepath
 		if(argc > 2)
 		{
 			string arg = argv[2];
@@ -44,65 +58,102 @@ int main(int argc, char *argv[])
 		return 1; // exit with errors
 	}
 	
-	////////// LEX //////////////////////////////////////////////////
-	
-	cout << endl << "Opening source program file..." << endl << endl;
-	
-	Lexer lex(source); // run the lexer by constructing one
-	queue<Token> que = lex.tokQue; // retreive the token queue
-	
-	if(lex.numErrors == -1) // file stream error
+	cout << endl << "Opening source program file..." << endl;
+	sourceFile.open(fileName.c_str()); // open the given file
+	if(sourceFile.fail()) // if the file cannot be opened
 	{
 		cout << "Error opening source program file. Please make sure the specified path to the file is correct." << endl; // print appropriate error message
-		return 1; // exit with errors
+		return 1; // exit with error
 	}
 	
-	// report lexical errors here
-	cout << endl << "[" << lex.numErrors << " lexical error(s) found.]"
-	<< " [" << lex.numWarnings << " lexical warning(s) found.]" << endl << endl;
+	// read source file into a string
+	string source((istreambuf_iterator<char>(sourceFile)), (istreambuf_iterator<char>()));
 	
-	// exit if lexical errors were found
-	if(lex.numErrors > 0)
+	// split the source into multiple programs (if necessary)
+	regex eof("([$]|[^$]+)"); // match anything and EOF and anything after
+	regex_iterator<string::iterator> regIt(source.begin(), source.end(), eof);
+	regex_iterator<string::iterator> regEnd;
+	string toAdd = "";
+	bool leftover = false;
+	queue<string> programs; // queue to hold all programs
+	while(regIt != regEnd)
 	{
-		return 1; // exit with errors
-	}
-	
-	// print out the token information if verbose is on
-	if(verbose)
-	{
-		cout <<
-			"______________________________________________________________________" << endl <<
-			setw(30) << left << "" << "TOKEN LIST" << setw(30) << right << "" << endl <<
-			"______________________________________________________________________" << endl;
-		while(!que.empty())
+		string txt = regIt->str();
+		if(txt == "$")
 		{
-			cout << left <<
-				"[NAME: " << setw(15) << que.front().name << "]" << 
-				"[VALUE: " << setw(10) << que.front().value << "]" << 
-				"[LINE: " << setw(20) << que.front().lineNum << "]" << endl;
-			que.pop();
+			programs.push(toAdd+"$");
+			toAdd = "";
+			leftover = false;
+		}else{
+			toAdd = txt;
+			leftover = true;
 		}
-		cout << "______________________________________________________________________" << endl;
+		++regIt;
 	}
-		
-	// indicate completion of lexical analysis
-	cout << endl << "Lexical Analysis complete!" << endl;
+	if(leftover) programs.push(toAdd); // add program with forgotten $
 	
-	////////// PARSE ////////////////////////////////////////////////
-	cout << endl << "Performing Parsing..." << endl << endl;
-	Parser parse(lex.tokQue, verbose); // run the Parser by constructing one
-	
-	// report parser errors here
-	cout << endl << "[" << parse.numErrors << " parse error(s) found.]" << endl;
-	
-	// exit if parser errors were found
-	if(parse.numErrors > 0)
+	// compile all the programs in order
+	int progNum = 1; // program number for keeping track of which is being compiled
+	while(!programs.empty())
 	{
-		return 1; // exit with errors
+		cout << " ____________________________" << endl << 
+		"| COMPILING PROGRAM No. " << setw(5) << progNum << "|" << endl <<
+		"+_____________________________________________________________________"<< endl;
+	
+		////////// LEX //////////////////////////////////////////////////
+		cout << "Performing Lexical Analysis..." << endl << endl;
+		Lexer lex(programs.front()); // run the lexer by constructing one
+		queue<Token> que = lex.tokQue; // retreive the token queue
+	
+		// report lexical errors here
+		cout << endl << "[" << lex.numErrors << " lexical error(s) found.]"
+		<< " [" << lex.numWarnings << " lexical warning(s) found.]" << endl << endl;
+	
+		// exit if lexical errors were found
+		if(lex.numErrors > 0)
+		{
+			return 1; // exit with errors
+		}
+	
+		// print out the token information if verbose is on
+		if(verbose)
+		{
+			cout <<
+				"______________________________________________________________________" << endl <<
+				setw(30) << left << "" << "TOKEN LIST" << setw(30) << right << "" << endl <<
+				"______________________________________________________________________" << endl;
+			while(!que.empty())
+			{
+				cout << left <<
+					"[NAME: " << setw(15) << que.front().name << "]" << 
+					"[VALUE: " << setw(10) << que.front().value << "]" << 
+					"[LINE: " << setw(20) << que.front().lineNum << "]" << endl;
+				que.pop();
+			}
+			cout << "______________________________________________________________________" << endl;
+		}
+		
+		// indicate completion of lexical analysis
+		cout << endl << "Lexical Analysis complete!" << endl;
+	
+		////////// PARSE ////////////////////////////////////////////////
+		cout << endl << "Performing Parsing..." << endl << endl;
+		Parser parse(lex.tokQue, verbose); // run the Parser by constructing one
+	
+		// report parser errors here
+		cout << endl << "[" << parse.numErrors << " parse error(s) found.]" << endl;
+	
+		// exit if parser errors were found
+		if(parse.numErrors > 0)
+		{
+			return 1; // exit with errors
+		}
+	
+		// indicate completion of parsing
+		cout << endl << "Parsing complete!" << endl;
+	
+		programs.pop(); // on to the next program
+		++progNum; // so increment the program number
 	}
-	
-	// indicate completion of parsing
-	cout << endl << "Parsing complete!" << endl;
-	
 	return 0; // exit successful
 }
--- lexer.h
+++ lexer.h
@@ -31,10 +31,11 @@ class Lexer
 // sourceFile	: the source program file the lexer should read from
 Lexer::Lexer(string sourceFile)
 {
-	// file input variables
-	ifstream source; // input file stream to read from the source file
-	string input; // stores what is being fed into this program from a file stream
-	char next; // the next character in the input stream
+	// program input variables
+	stringstream source; // string stream to read from the source program
+	source.str(sourceFile);
+	// string input; // stores what is being fed into this program from a string stream
+	char next = source.get(); // the next character in the input stream
 	
 	// other important variables
 	int lineNum = 1; // the line number the parser is currently on
@@ -80,267 +81,261 @@ Lexer::Lexer(string sourceFile)
 		{0,0,0,0,0,0,0,0,0,0,0,0,0,30,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}, // State : 29
 		{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0} // State : 30
 	};
+
+	// store the next character in next and set loop to false if this is the end of file
+	// static casting to correct Microsoft VS 2013 compiler istream to bool conversion error
+	//bool loop = static_cast<bool> (source.get(next)); 
 	
-	source.open(sourceFile.c_str()); // open the given file
-	if(source.fail()) // if the file cannot be opened
+	readingCharList = false; // true if the lexer should be reading a CharList
+	while(next != EOF)
 	{
-		numErrors = -1; // specifies there is a file stream error
-		return; // exit with error
-	}
-	
-	cout << "Performing Lexical Analysis..." << endl << endl;
-	
-	if(source.is_open())
-	{
-		// store the next character in next and set loop to false if this is the end of file
-		// static casting to correct Microsoft VS 2013 compiler istream to bool conversion error
-		bool loop = static_cast<bool> (source.get(next)); 
-		readingCharList = false; // true if the lexer should be reading a CharList
-		while(loop)
+		// cout << next;
+		bool error = false; // becomes true if a character found is not a part of the grammar
+		bool newline = false; // becomes true if a character found is the newline character
+		bool spaceChar = false; // becomes true if a space is found in a CharList
+		// get the int counterpart to the current scanned char for state table traversal
+		switch(next)
 		{
-			bool error = false; // becomes true if a character found is not a part of the grammar
-			bool newline = false; // becomes true if a character found is the newline character
-			bool spaceChar = false; // becomes true if a space is found in a CharList
-			// get the int counterpart to the current scanned char for state table traversal
-			switch(next)
-			{
-				case 'a':
-					i = 0;
-					break;
-				case 'b':
-					i = 1;
-					break;
-				case 'c':
-					i = 2;
-					break;
-				case 'd':
-					i = 3;
-					break;
-				case 'e':
-					i = 4;
-					break;
-				case 'f':
-					i = 5;
-					break;
-				case 'g':
-					i = 6;
-					break;
-				case 'h':
-					i = 7;
-					break;
-				case 'i':
-					i = 8;
-					break;
-				case 'j':
-					i = 9;
-					break;
-				case 'k':
-					i = 10;
-					break;
-				case 'l':
-					i = 11;
-					break;
-				case 'm':
-					i = 12;
-					break;
-				case 'n':
-					i = 13;
-					break;
-				case 'o':
-					i = 14;
-					break;
-				case 'p':
-					i = 15;
-					break;
-				case 'q':
-					i = 16;
-					break;
-				case 'r':
-					i = 17;
-					break;
-				case 's':
-					i = 18;
-					break;
-				case 't':
-					i = 19;
-					break;
-				case 'u':
-					i = 20;
-					break;
-				case 'v':
-					i = 21;
-					break;
-				case 'w':
-					i = 22;
-					break;
-				case 'x':
-					i = 23;
-					break;
-				case 'y':
-					i = 24;
-					break;
-				case 'z':
-					i = 25;
-					break;
-				case '0':
-					i = 26;
-					break;
-				case '1':
-					i = 27;
-					break;
-				case '2':
-					i = 28;
-					break;
-				case '3':
-					i = 29;
-					break;
-				case '4':
-					i = 30;
-					break;
-				case '5':
-					i = 31;
-					break;
-				case '6':
-					i = 32;
-					break;
-				case '7':
-					i = 33;
-					break;
-				case '8':
-					i = 34;
-					break;
-				case '9':
-					i = 35;
-					break;
-				case '+':
-					i = 36;
-					break;
-				case '{':
-					i = 37;
-					break;
-				case '}':
-					i = 38;
-					break;
-				case '(':
-					i = 39;
-					break;
-				case ')':
-					i = 40;
-					break;
-				case '"':
-					i = 41;
-					break;
-				case '$':
-					i = 42;
-					break;
-				case '=':
-					i = 43;
-					break;
-				case '!':
-					i = 44;
-					break;
-				case '\n': // newline
-				case '\r': // carriage return (newline)
-					if(readingCharList) // newlines not allowed in CharLists
-					{
-						cout << "[ERROR]Line " << lineNum << ": " << "The newline character is not a valid character token." << endl;
-						++numErrors; // increment the number of errors found
-					}
-					newline = true;
-					break;
-				case ' ':
-					if(!readingCharList) // is this space not part of a CharList?
-					{
-						loop = static_cast<bool> (source.get(next)); // go on to the next character
-						continue; // jump to the end of this loop iteration
-					}
-					// this space IS part of a CharList
-					spaceChar = true;
-					break;
-				case '\t':
-					if(readingCharList) // tabs not allowed in CharLists
-					{
-						cout << "[ERROR]Line " << lineNum << ": " << "The tab character is not a valid character token." << endl;
-						++numErrors; // increment the number of errors found
-						error = true;
-					}
-					loop = static_cast<bool> (source.get(next)); // go on to the next character
+			case 'a':
+				i = 0;
+				break;
+			case 'b':
+				i = 1;
+				break;
+			case 'c':
+				i = 2;
+				break;
+			case 'd':
+				i = 3;
+				break;
+			case 'e':
+				i = 4;
+				break;
+			case 'f':
+				i = 5;
+				break;
+			case 'g':
+				i = 6;
+				break;
+			case 'h':
+				i = 7;
+				break;
+			case 'i':
+				i = 8;
+				break;
+			case 'j':
+				i = 9;
+				break;
+			case 'k':
+				i = 10;
+				break;
+			case 'l':
+				i = 11;
+				break;
+			case 'm':
+				i = 12;
+				break;
+			case 'n':
+				i = 13;
+				break;
+			case 'o':
+				i = 14;
+				break;
+			case 'p':
+				i = 15;
+				break;
+			case 'q':
+				i = 16;
+				break;
+			case 'r':
+				i = 17;
+				break;
+			case 's':
+				i = 18;
+				break;
+			case 't':
+				i = 19;
+				break;
+			case 'u':
+				i = 20;
+				break;
+			case 'v':
+				i = 21;
+				break;
+			case 'w':
+				i = 22;
+				break;
+			case 'x':
+				i = 23;
+				break;
+			case 'y':
+				i = 24;
+				break;
+			case 'z':
+				i = 25;
+				break;
+			case '0':
+				i = 26;
+				break;
+			case '1':
+				i = 27;
+				break;
+			case '2':
+				i = 28;
+				break;
+			case '3':
+				i = 29;
+				break;
+			case '4':
+				i = 30;
+				break;
+			case '5':
+				i = 31;
+				break;
+			case '6':
+				i = 32;
+				break;
+			case '7':
+				i = 33;
+				break;
+			case '8':
+				i = 34;
+				break;
+			case '9':
+				i = 35;
+				break;
+			case '+':
+				i = 36;
+				break;
+			case '{':
+				i = 37;
+				break;
+			case '}':
+				i = 38;
+				break;
+			case '(':
+				i = 39;
+				break;
+			case ')':
+				i = 40;
+				break;
+			case '"':
+				i = 41;
+				break;
+			case '$':
+				i = 42;
+				break;
+			case '=':
+				i = 43;
+				break;
+			case '!':
+				i = 44;
+				break;
+			case '\n': // newline
+			case '\r': // carriage return (newline)
+				if(readingCharList) // newlines not allowed in CharLists
+				{
+					cout << "[ERROR]Line " << lineNum << ": " << "The newline character is not a valid character token." << endl;
+					++numErrors; // increment the number of errors found
+				}
+				newline = true;
+				break;
+			case ' ':
+				if(!readingCharList) // is this space not part of a CharList?
+				{
+					//loop = static_cast<bool> (source.get(next)); // go on to the next character
+					next = source.get();
 					continue; // jump to the end of this loop iteration
-					break;
-				default:
-					error = true; // this character is not found in the grammar
-					break;
-			}
-			
-			int fromState = state; // for testing
-			if(error || newline || spaceChar)
-				state = 0;
-			else
-				state = transitionTable[state][i];
-			tokVal += next; // add the next character to the token value
-			
-			// cout << "PREV STATE: [" << fromState << "] NEW STATE: [" << state << "] CHAR: [" << next << "]" << endl;
-			
-			switch(state)
-			{
-				case 0: // we arrive here either due to the start of a new token or an error being found
-					int max; // number of characters to loop through in tokVal
-					if(tokVal.size() == 1) 
-					{
-						max = 1; // iterate once if there is only one character in tokVal
-						loop = static_cast<bool> (source.get(next)); // go on to the next character
-					}
-					else 
-						max = tokVal.size()-1; // iterate through this loop for every character in tokVal except the last
-					for(string::size_type j = 0; j < max; ++j)
-					{
-						char& c = tokVal[j];
-						if((c >= 'a' && c <= 'z') || c == '=' || c == ' ') // if the character c is a lowercase letter a-z OR '=' OR ' ' (space char)
-						{
-							// cout << "Definite " << c << endl;
-							string s; // string to hold the single character
-							stringstream ss; // string stream for converting single characters into strings
-							ss << c; // send character c to string stream ss 
-							ss >> s; // stream string into string s
-							string name; // name of the token
-							if(c == '=') name = "T_ASSIGN";
-							else if(c == ' ') name = "T_SPACE";
-							else name = "T_ID";
-							Token tok = {s, name, lineNum}; // create a new token
-							tokQue.push(tok); // append token to the token quetor
-						}
-						else if(c == '\n' || c == '\r') // new line 
-						{
-							lineNum++; // incremenet the current line number
-							// also effectively ends token analysis at the newline char
-						}					
-						else
-						{
-							cout << "[ERROR]Line " << lineNum << ": " << c << " is not a valid lexeme." << endl;
-							++numErrors; // increment the number of errors found
-						}
-					}
-					tokVal = ""; // reset the token name
-					continue; // jump to the end of this loop iteration
-					break;
-				case 1: // this state means we've found an id
-					// cout << "Definite id " << tokVal << endl;
-					addToken(tokQue, tokVal, "T_ID", lineNum, state);
-					break;
-				case 2: // this state means we've found an integer
-					// cout << "Definite integer " << tokVal << endl;
-					addToken(tokQue, tokVal, "T_DIGIT", lineNum, state);
-					break;
-				case 30: // this state means we've found a reserve word (e.g. print)
-					// cout << "Definite reserved word " << tokVal << endl;
-					addToken(tokQue, tokVal, getTokenName(tokVal), lineNum, state);
-					break;
-				default:
-					// do nothing - we are not in an accepting state
-					break;
-			}
-			loop = static_cast<bool> (source.get(next)); // go on to the next character
+				}
+				// this space IS part of a CharList
+				spaceChar = true;
+				break;
+			case '\t':
+				if(readingCharList) // tabs not allowed in CharLists
+				{
+					cout << "[ERROR]Line " << lineNum << ": " << "The tab character is not a valid character token." << endl;
+					++numErrors; // increment the number of errors found
+					error = true;
+				}
+				//loop = static_cast<bool> (source.get(next)); // go on to the next character
+				next = source.get();
+				continue; // jump to the end of this loop iteration
+				break;
+			default:
+				error = true; // this character is not found in the grammar
+				break;
 		}
+			
+		int fromState = state; // for testing
+		if(error || newline || spaceChar)
+			state = 0;
+		else
+			state = transitionTable[state][i];
+		tokVal += next; // add the next character to the token value
+			
+		// cout << "PREV STATE: [" << fromState << "] NEW STATE: [" << state << "] CHAR: [" << next << "]" << endl;
+			
+		switch(state)
+		{
+			case 0: // we arrive here either due to the start of a new token or an error being found
+				int max; // number of characters to loop through in tokVal
+				if(tokVal.size() == 1) 
+				{
+					max = 1; // iterate once if there is only one character in tokVal
+					//loop = static_cast<bool> (source.get(next)); // go on to the next character
+					next = source.get();
+				}
+				else 
+					max = tokVal.size()-1; // iterate through this loop for every character in tokVal except the last
+				for(string::size_type j = 0; j < max; ++j)
+				{
+					char& c = tokVal[j];
+					if((c >= 'a' && c <= 'z') || c == '=' || c == ' ') // if the character c is a lowercase letter a-z OR '=' OR ' ' (space char)
+					{
+						// cout << "Definite " << c << endl;
+						string s; // string to hold the single character
+						stringstream ss; // string stream for converting single characters into strings
+						ss << c; // send character c to string stream ss 
+						ss >> s; // stream string into string s
+						string name; // name of the token
+						if(c == '=') name = "T_ASSIGN";
+						else if(c == ' ') name = "T_SPACE";
+						else name = "T_ID";
+						Token tok = {s, name, lineNum}; // create a new token
+						tokQue.push(tok); // append token to the token quetor
+					}
+					else if(c == '\n' || c == '\r') // new line 
+					{
+						lineNum++; // incremenet the current line number
+						// also effectively ends token analysis at the newline char
+					}					
+					else
+					{
+						cout << "[ERROR]Line " << lineNum << ": " << c << " is not a valid lexeme." << endl;
+						++numErrors; // increment the number of errors found
+					}
+				}
+				tokVal = ""; // reset the token name
+				continue; // jump to the end of this loop iteration
+				break;
+			case 1: // this state means we've found an id
+				// cout << "Definite id " << tokVal << endl;
+				addToken(tokQue, tokVal, "T_ID", lineNum, state);
+				break;
+			case 2: // this state means we've found an integer
+				// cout << "Definite integer " << tokVal << endl;
+				addToken(tokQue, tokVal, "T_DIGIT", lineNum, state);
+				break;
+			case 30: // this state means we've found a reserve word (e.g. print)
+				// cout << "Definite reserved word " << tokVal << endl;
+				addToken(tokQue, tokVal, getTokenName(tokVal), lineNum, state);
+				break;
+			default:
+				// do nothing - we are not in an accepting state
+				break;
+		}
+		//loop = static_cast<bool> (source.get(next)); // go on to the next character
+		next = source.get();
 	}
 	
 	// handle lex warnings
@@ -366,8 +361,6 @@ Lexer::Lexer(string sourceFile)
 			++numWarnings; // increment number of warnings
 		}
 	}
-	
-	source.close(); // close the file input streams
 }
 
 // function to add a token to the token quetor
